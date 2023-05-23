0f37a4a44660865a1430e866d914daa63dbd571f
william <wilsher987@gmail.com>
2023-05-22 22:56:27 -0500

    leader

--- src/js/team.js
+++ src/js/team.js
@@ -3,7 +3,7 @@ export const members = [
     "name": "David",
     "surname": "Vargas",
     "url": "https://github.com/bilisito98",
-    "role": ["Team Lead", "Web Developer"],
+    "role": ["Team Leader", "Fullstack Developer"],
     "photo": "https://avatars.githubusercontent.com/u/102624868?s=400&u=cdeacadc7ff36b27bb342e227054c2c0ba511458&v=4"
   }
 ]
