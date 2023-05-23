e2c22d4f5ea655d51c78f540a9653a750ad45bdb
william <wilsher987@gmail.com>
2023-05-22 22:55:26 -0500

    committ

--- src/js/team.js
+++ src/js/team.js
@@ -1,9 +1,9 @@
 export const members = [
   {
-    "name": "William",
+    "name": "David",
     "surname": "Vargas",
     "url": "https://github.com/bilisito98",
     "role": ["Team Lead", "Web Developer"],
-    "photo": "https://avatars.githubusercontent.com/u/102624868?v=4"
+    "photo": "https://avatars.githubusercontent.com/u/102624868?s=400&u=cdeacadc7ff36b27bb342e227054c2c0ba511458&v=4"
   }
 ]
